9e4599d97bc7b8b9d9bff0f09f785f537f3349c5
Kisung Son <eson@samsung.com>
2018-11-05 14:02:30 +0900

    add a project IoTProject Base code project

---
+++ IoTProject/inc/iotproject.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __IOTPROJECT_H__
+#define __IOTPROJECT_H__
+
+
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "IOTPJT"
+
+
+#endif /* __IOTPROJECT_H__ */
---
+++ IoTProject/src/iotproject.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Flora License, Version 1.1 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://floralicense.org/license/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <tizen.h>
+#include <service_app.h>
+
+#include <iotproject.h>
+
+#include "log.h"
+
+
+bool service_app_create(void *data)
+{
+	return true;
+}
+
+void service_app_terminate(void *data)
+{
+	FN_CALL;
+}
+
+void service_app_control(app_control_h app_control, void *data)
+{
+	FN_CALL;
+}
+
+int main(int argc, char* argv[])
+{
+	FN_CALL;
+
+	char ad[50] = {0,};
+	service_app_lifecycle_callback_s event_callback;
+
+	event_callback.create = service_app_create;
+	event_callback.terminate = service_app_terminate;
+	event_callback.app_control = service_app_control;
+
+	return service_app_main(argc, argv, &event_callback, ad);
+}
